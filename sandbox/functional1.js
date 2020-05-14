77d7abf6eed76f2201d83b33b607f84f07f80021
Mike Williams <mumakin@gmail.com>
2020-05-13 22:04:57 -0600

    using function expressions

--- sandbox/functional1.js
+++ sandbox/functional1.js
@@ -2,3 +2,20 @@ function myLog(message){
     console.log(message);
 }
 myLog('hi');
+
+function doesWork(logger){
+    var a = 1;
+    logger(a);
+    var b = 2;
+    logger(b);
+    var c = b + a;
+    logger(c);
+}
+
+doesWork(myLog);
+
+var functionExpressionLogger = function(message){
+    console.log('fe' + message);
+}
+
+doesWork(functionExpressionLogger);

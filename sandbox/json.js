e3deb848bf843cd4297aa1fbf147eb30eda37a68
Mike Williams <mumakin@gmail.com>
2020-05-13 22:18:34 -0600

    do json in js

---
+++ sandbox/json.js
@@ -0,0 +1,13 @@
+var person = {
+    firstname: 'John',
+    lastname: 'Doe',
+    greet: function(){
+        console.log('hello, ' + this.firstname + ' ' + this.lastname);
+    }
+}
+
+person.greet();
+
+person.firstname = 'Susan';
+person.lastname = 'Smith';
+person.greet();

d654544dfbb74d6cd132e3effd62977787bab9c9
Mike Williams <mumakin@gmail.com>
2020-05-13 22:26:52 -0600

    prototypes

--- sandbox/app.js
+++ sandbox/app.js
@@ -9,5 +9,10 @@ function Person(firstname, lastname) {
     this.lastname = lastname;
 }
 
+Person.prototype.greet = function(){
+    console.log('Hello, ' + this.firstname + ' ' + this.lastname);
+}
+
 var john = new Person('John', 'Doe');
 console.log(john.firstname + ' ' + john.lastname);
+john.greet();

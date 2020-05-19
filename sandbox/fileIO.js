1563785c823e5f2a44132f0af05fe146c52db434
Mike Williams <mumakin@gmail.com>
2020-05-18 22:20:20 -0600

    file stream

--- sandbox/fileIO.js
+++ sandbox/fileIO.js
@@ -6,10 +6,19 @@ var greetFile = fs.readFile(__dirname + '/greetFile.txt', 'utf8',
             console.log(error);
             return;
         }
-        console.log('\n\n\n' + fileContents + '\n\n\n');
+        console.log('\nasync file read complete \n')
+        console.log(fileContents + '\n');
 });
 // no matter how long my loop, the callback happens after. There is probably a keyword here I need
 var i;
 for(i = 0; i < 40; ++i){
     console.log(i + 'doing other thingst that might happen before the file is read');
 }
+
+// now lets read it as a file stream
+var fileStream = fs.createReadStream(__dirname + '/greetFile.txt', { encoding: 'utf8', highWaterMark: 6});
+fileStream.callCount = 0;
+fileStream.on('data', function(chunk){
+    this.callCount++;
+    console.log(`new chunk ${this.callCount}: ${chunk}`);
+});

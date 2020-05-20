56c3b661d13c153d5fa5fe4658638a5682b0474d
Mike Williams <mumakin@gmail.com>
2020-05-19 22:04:05 -0600

    use streams for async server

--- sandbox/myFirstWepServer.js
+++ sandbox/myFirstWepServer.js
@@ -4,6 +4,5 @@ var fs = require('fs');
 http.createServer(function(request, response){
     console.log(`Got request: ${request}`);
     response.writeHead(200, {'Content-Type': 'text/html'});
-    var html = fs.readFileSync(__dirname + '/first.html');
-    response.end(html);
+    fs.createReadStream(__dirname + '/first.html').pipe(response);
 }).listen(8080, '127.0.0.1');

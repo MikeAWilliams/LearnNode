8bf72f5d110d6d78eb51aa0c0e1792e73e85216d
Mike Williams <mumakin@gmail.com>
2020-05-19 21:57:38 -0600

    html support!

--- sandbox/myFirstWepServer.js
+++ sandbox/myFirstWepServer.js
@@ -1,7 +1,9 @@
 var http = require('http');
+var fs = require('fs');
 
 http.createServer(function(request, response){
     console.log(`Got request: ${request}`);
-    response.writeHead(200, {'Content-Type': 'text/plain'});
-    response.end('Hello World!\n');
+    response.writeHead(200, {'Content-Type': 'text/html'});
+    var html = fs.readFileSync(__dirname + '/first.html');
+    response.end(html);
 }).listen(8080, '127.0.0.1');

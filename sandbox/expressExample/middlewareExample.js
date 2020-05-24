e495f7502ae822048e898821e9e42382019c88e3
Mike Williams <mumakin@gmail.com>
2020-05-23 22:17:36 -0600

    adding post

--- sandbox/expressExample/middlewareExample.js
+++ sandbox/expressExample/middlewareExample.js
@@ -1,5 +1,8 @@
 var express = require('express');
+var bodyParser = require('body-parser')
 var app = express();
+
+var urlencodedParser = bodyParser.urlencoded({extended:false});
 
 app.set('view engine', 'ejs');
 
@@ -14,12 +17,14 @@ app.get('/', function(request, result){
     result.render('index');
 });
 
-app.get('/person', function(request, result){
-    result.render('person', {ID: request.params});
+app.post('/person', urlencodedParser, function(request, result){
+    result.send('Thank you!');
+    console.log(request.body.firstname);
+    console.log(request.body.lastname);
 });
 
 app.get('/person/:id', function(request, result){
-    result.render('person', {ID: request.params.id});
+    result.render('person', {ID: request.params.id, Qstr: request.query.name});
 });
 
 // consider express middleware passport for authentication

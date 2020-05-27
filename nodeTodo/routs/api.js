e6603af4f3f10f9b7f21786088410f64ca197ec8
Mike Williams <mumakin@gmail.com>
2020-05-26 23:08:04 -0600

    add more apis

--- nodeTodo/routs/api.js
+++ nodeTodo/routs/api.js
@@ -5,9 +5,48 @@ module.exports = function(app, todoModel){
     app.use(bodyParser.urlencoded({extended:true}));
 
     app.get('/api/todos/:username', function(request, response){
-        todoModel.find({username: request.params.username}, function(error, todos){
+        todoModel.find({username: request.params.username}, function(error, todosResult){
             if(error) throw error;
-            response.send(todos);
+            response.send(todosResult);
+        });
+    });
+
+    app.get('/api/todo/:id', function(request, response){
+        todoModel.findById({ _id: request.params.id}, function(error, todoResult){
+            if(error) throw error;
+            response.send(todoResult);
+        })
+    });
+
+    app.post('/api/todo', function(request, response){
+        if(request.body.id){
+            todoModel.findByIdAndUpdate(request.body.id, 
+                {
+                    todo: request.body.todo,
+                    isDone: request.body.isDone
+                }, 
+                function(error, todoResult){
+                    if(error) throw error;
+                    response.send('item updated');
+                });
+        }
+        else {
+            var newTodo = todoModel({
+                username: 'test',
+                todo: request.body.todo,
+                isDone: request.body.isDone
+            });
+            newTodo.save(function(error){
+                if(error) throw error;
+                response.send('new item created')
+            });
+        }
+    });
+
+    app.delete('/api/todo', function(request, response){
+        todoModel.findByIdAndRemove(request.bodyParser.id, function(error){
+            if(error) throw error;
+            response.send('Item delted');
         });
     });
 };
